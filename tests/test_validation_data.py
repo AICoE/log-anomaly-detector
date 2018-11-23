92710e3c23aba8dec295d716ed55167affa2185d
Zak Hassan <zak.hassan1010@gmail.com>
2018-11-23 10:35:28 -0500

    Created initial pytest unit test to validate that training on public dataset (#47)
    * Adding simple unit test to validate that the anomaly detection training completes successfully

---
+++ tests/test_validation_data.py
@@ -0,0 +1,20 @@
+import pytest
+from anomaly_detector.anomaly_detector import AnomalyDetector
+from anomaly_detector.config import Configuration
+
+CONFIGURATION_PREFIX = "LAD"
+
+@pytest.fixture()
+def detector():
+    config = Configuration(CONFIGURATION_PREFIX)
+    anomaly_detector = AnomalyDetector(config)
+    return anomaly_detector
+
+def test_end2endtraining(detector):
+    """
+    Test_End2EndTraining
+
+    Test anomaly detection training on public dataset to confirm that the training completes successfully end2end
+
+    """
+    assert detector.train() == 0
