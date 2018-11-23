78b3a03e07b21a63faeac1eb073bcd17d55b0f42
Zak Hassan <zak.hassan1010@gmail.com>
2018-11-23 10:31:35 -0500

    Removing unnecessary logging

--- tests/test_validation_data.py
+++ tests/test_validation_data.py
@@ -1,30 +1,11 @@
 import pytest
-
 from anomaly_detector.anomaly_detector import AnomalyDetector
-import sys
-import logging
-
 from anomaly_detector.config import Configuration
-
-_LOGGER = logging.getLogger()
-_LOGGER.setLevel(logging.INFO)
-
-# create a logging format
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-sh = logging.StreamHandler(sys.stdout)
-sh.setFormatter(formatter)
-_LOGGER.addHandler(sh)
-
-w2v_logger = logging.getLogger('gensim.models')
-w2v_logger.setLevel(logging.ERROR)
 
 CONFIGURATION_PREFIX = "LAD"
 
-
 @pytest.fixture()
 def detector():
-    _LOGGER.info("Starting...")
     config = Configuration(CONFIGURATION_PREFIX)
     anomaly_detector = AnomalyDetector(config)
     return anomaly_detector
@@ -37,5 +18,3 @@ def test_end2endtraining(detector):
 
     """
     assert detector.train() == 0
-
-
