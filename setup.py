ba368d0b0f64da713d39f7b8653ee2715ea5b256
Zak Hassan <zak.hassan1010@gmail.com>
2019-03-14 20:50:09 -0400

    Adding git dependency to be installed via setup.pyas advised

--- setup.py
+++ setup.py
@@ -1,19 +1,13 @@
 """ Setup.py for packaging log-anomaly-detector as library """
-import os
 from setuptools import setup
 
-os.system('pip install git+https://github.com/sevamoo/SOMPY.git' +
-          '@76b60ebd6ffd550b0f7faaf632451dfd68827bf7')
-os.system('pip install ' +
-          'git+https://github.com/jaybaird/python-bloomfilter.git' +
-          '@2bbe01ad49965bf759e31781e6820408068862ac')
-REQUIRED_PKG = ["elasticsearch5", "gensim", "matplotlib", "numpy", "pandas",
-                "prometheus_client", "boto3", "Flask", "fastparquet",
-                "scikit-learn", "scipy", "tqdm",
-                "s3fs", "sompy", "pybloom"]
+REQUIRED_PKG = ["elasticsearch5", "gensim", "matplotlib", "numpy",
+                "pandas", "prometheus_client", "boto3", "Flask", "fastparquet",
+                "scikit-learn", "scipy", "tqdm", "s3fs",
+                "sompy", "pybloom"]
 
 setup(
-    name='lad-core',
+    name='lad-anomaly-detector',
     version='0.0.1',
     packages=['fact_store',
               'anomaly_detector',
@@ -33,6 +27,12 @@ setup(
     license='',
     author='AiOps',
     author_email='zak.hassan@redhat.com',
-    description='Log anomaly detector for streaming logs in kubernetes',
+    description='Log anomaly detector for streaming logs',
+    dependency_links=[
+        'git+https://github.com/sevamoo/SOMPY.git' +
+        '@76b60ebd6ffd550b0f7faaf632451dfd68827bf7',
+        'git+https://github.com/jaybaird/python-bloomfilter.git' +
+        '@2bbe01ad49965bf759e31781e6820408068862ac'
+    ],
     install_requires=REQUIRED_PKG
 )
