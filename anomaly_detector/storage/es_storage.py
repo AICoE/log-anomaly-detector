4da6f44027dcd0fef595664831491fc0a6102c73
Zak Hassan <zak.hassan1010@gmail.com>
2019-05-16 11:32:04 -0400

    Fixing elasticsearch configurations to allow for configmap instead of hardcoded env var (#120)

--- anomaly_detector/storage/es_storage.py
+++ anomaly_detector/storage/es_storage.py
@@ -24,37 +24,36 @@ class ESStorage(Storage):
 
     def __init__(self, configuration):
         """Initialize Elasticsearch storage backend."""
-        super(ESStorage, self).__init__(configuration)
-        self.config.storage = ESConfiguration()
+        self.config = configuration
         self._connect()
 
     def _connect(self):
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-        if len(self.config.storage.ES_CERT_DIR) and os.path.isdir(self.config.storage.ES_CERT_DIR):
+        if len(self.config.ES_CERT_DIR) and os.path.isdir(self.config.ES_CERT_DIR):
             _LOGGER.warn(
                 "Using cert and key in %s for connection to %s (verify_certs=%s)."
                 % (
-                    self.config.storage.ES_CERT_DIR,
-                    self.config.storage.ES_ENDPOINT,
-                    self.config.storage.ES_VERIFY_CERTS,
+                    self.config.ES_CERT_DIR,
+                    self.config.ES_ENDPOINT,
+                    self.config.ES_VERIFY_CERTS,
                 )
             )
             self.es = Elasticsearch(
-                self.config.storage.ES_ENDPOINT,
-                use_ssl=self.config.storage.ES_USE_SSL,
-                verify_certs=self.config.storage.ES_VERIFY_CERTS,
-                client_cert=os.path.join(self.config.storage.ES_CERT_DIR, "es.crt"),
-                client_key=os.path.join(self.config.storage.ES_CERT_DIR, "es.key"),
+                self.config.ES_ENDPOINT,
+                use_ssl=self.config.ES_USE_SSL,
+                verify_certs=self.config.ES_VERIFY_CERTS,
+                client_cert=os.path.join(self.config.ES_CERT_DIR, "es.crt"),
+                client_key=os.path.join(self.config.ES_CERT_DIR, "es.key"),
                 timeout=60,
                 max_retries=2,
             )
         else:
             _LOGGER.warn("Conecting to ElasticSearch without authentication.")
-            print(self.config.storage.ES_USE_SSL)
+            print(self.config.ES_USE_SSL)
             self.es = Elasticsearch(
-                self.config.storage.ES_ENDPOINT,
-                use_ssl=self.config.storage.ES_USE_SSL,
-                verify_certs=self.config.storage.ES_VERIFY_CERTS,
+                self.config.ES_ENDPOINT,
+                use_ssl=self.config.ES_USE_SSL,
+                verify_certs=self.config.ES_VERIFY_CERTS,
                 timeout=60,
                 max_retries=2,
             )
@@ -68,7 +67,7 @@ class ESStorage(Storage):
 
     def retrieve(self, time_range: int, number_of_entires: int):
         """Retrieve data from ES."""
-        index_in = self._prep_index_name(self.config.storage.ES_INPUT_INDEX)
+        index_in = self._prep_index_name(self.config.ES_INPUT_INDEX)
 
         query = {
             "sort": {"@timestamp": {"order": "desc"}},
@@ -86,12 +85,12 @@ class ESStorage(Storage):
             "Reading in max %d log entries in last %d seconds from %s",
             number_of_entires,
             time_range,
-            self.config.storage.ES_ENDPOINT,
+            self.config.ES_ENDPOINT,
         )
 
         query["size"] = number_of_entires
         query["query"]["bool"]["must"][1]["range"]["@timestamp"]["gte"] = "now-%ds" % time_range
-        query["query"]["bool"]["must"][0]["query_string"]["query"] = self.config.storage.ES_QUERY
+        query["query"]["bool"]["must"][0]["query_string"]["query"] = self.config.ES_QUERY
 
         es_data = self.es.search(index_in, body=json.dumps(query))
         if es_data["hits"]["total"] == 0:
@@ -108,31 +107,8 @@ class ESStorage(Storage):
 
     def store_results(self, data):
         """Store results back to ES."""
-        index_out = self._prep_index_name(self.config.storage.ES_TARGET_INDEX)
+        index_out = self._prep_index_name(self.config.ES_TARGET_INDEX)
 
         actions = [{"_index": index_out, "_type": "log", "_source": data[i]} for i in range(len(data))]
 
         helpers.bulk(self.es, actions, chunk_size=int(len(data) / 4) + 1)
-
-
-class ESConfiguration(Configuration):
-    """Elasticsearch configuration."""
-
-    # ElasticSearch endpoint URL
-    ES_ENDPOINT = ""
-    # Path to a directory where cert and key (es.crt and es.key) are stored for authentication
-    ES_CERT_DIR = ""
-    # If True, connect using ssl
-    ES_USE_SSL = True
-    # If True, verify SSL certificates
-    ES_VERIFY_CERTS = False
-    # ElasticSearch index name where results will be pushed to
-    ES_TARGET_INDEX = ""
-    # ElasticSearch index name where log entries will be pulled from
-    ES_INPUT_INDEX = ""
-    # JSON representing a query passed to ElasticSearch to match the data
-    ES_QUERY = ""
-
-    def __init__(self):
-        """Initialize ES configuration."""
-        self.load()
