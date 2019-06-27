18fd8b9a52b6cb4754a7d267cecac901ac85b0c5
Michael Clifford <mcliffor@redhat.com>
2019-06-27 14:00:34 -0400

    additional exception classes

--- anomaly_detector/exception/exceptions.py
+++ anomaly_detector/exception/exceptions.py
@@ -1,9 +1,25 @@
-"""Fact Store Exception - When required env vars are not set we throw these exceptions."""
+"""Exceptions for the log anomaly detector."""
 
 
 class factStoreEnvVarNotSetException(Exception):
     """Fact Store env var validator."""
 
     def __init__(self, msg="fact store url env var not set"):
+        """Initialize message."""
+        self.message = msg
+
+
+class ModelLoadException(Exception):
+    """Validates that model has been loaded."""
+
+    def __intit__(self, msg="There is no existing model to load"):
+        """Initialize message."""
+        self.message = msg
+
+
+class ModelSaveException(Exception):
+    """Validates that model has been saved."""
+
+    def __intit__(self, msg="The model could not be saved"):
         """Initialize message."""
         self.message = msg
