62bfbcf7ee8744300f2e138e4156296b23d8a72f
Zak Hassan <zak.hassan1010@gmail.com>
2019-06-28 16:57:40 -0400

    adding better observability in refactored code

--- anomaly_detector/adapters/som_model_adapter.py
+++ anomaly_detector/adapters/som_model_adapter.py
@@ -10,6 +10,13 @@ from anomaly_detector.model.model_exception import ModelLoadException, ModelSave
 from anomaly_detector.model.sompy_model import SOMPYModel
 from anomaly_detector.model.w2v_model import W2VModel
 import os
+from prometheus_client import Gauge, Summary, Counter, Histogram
+
+ANOMALY_COUNT = Gauge("aiops_lad_anomaly_count", "count of anomalies runs", ['anomaly_status'])
+LOG_LINES_COUNT = Gauge("aiops_lad_loglines_count", "count of log lines processed runs")
+FALSE_POSITIVE_COUNT = Counter("aiops_lad_false_positive_count", "count of false positives processed runs", ['id'])
+ANOMALY_HIST = Histogram("aiops_hist", "histogram of anomalies runs")
+THRESHOLD = Gauge("aiops_lad_threshold", "Threshold of marker for anomaly")
 
 
 class SomModelAdapter(BaseModelAdapter):
@@ -63,15 +70,18 @@ class SomModelAdapter(BaseModelAdapter):
     def preprocess(self, config_type, recreate_model):
         """Load data and train."""
         data, raw = self.storage_adapter.load_data(config_type)
-        if not recreate_model:
-            self.w2v_model.update(data)
-        else:
-            self.w2v_model.create(data, self.storage_adapter.TRAIN_VECTOR_LENGTH, self.storage_adapter.TRAIN_WINDOW)
-        try:
-            self.w2v_model.save(self.storage_adapter.W2V_MODEL_PATH)
-        except ModelSaveException as ex:
-            logging.error("Failed to save W2V model: %s" % ex)
-            raise
+        # if data:
+        if data is not None:
+            LOG_LINES_COUNT.set(len(data))
+            if not recreate_model:
+                self.w2v_model.update(data)
+            else:
+                self.w2v_model.create(data, self.storage_adapter.TRAIN_VECTOR_LENGTH, self.storage_adapter.TRAIN_WINDOW)
+            try:
+                self.w2v_model.save(self.storage_adapter.W2V_MODEL_PATH)
+            except ModelSaveException as ex:
+                logging.error("Failed to save W2V model: %s" % ex)
+                raise
 
         return data, raw
 
@@ -82,6 +92,7 @@ class SomModelAdapter(BaseModelAdapter):
         false_positives = feedback_strategy.execute() if feedback_strategy else None
         dist = self.process_anomaly_score(data)
         f = []
+        hist_count = 0
         logging.info("Max anomaly score: %f" % max(dist))
         for i in range(len(data)):
             s = json_logs[i]
@@ -91,15 +102,20 @@ class SomModelAdapter(BaseModelAdapter):
             if false_positives is not None:
                 if {"message": s["message"]} in false_positives:
                     logging.info("False positive was found (score: %f): %s" % (dist[i], s["message"]))
+                    FALSE_POSITIVE_COUNT.labels(id=s["predict_id"]).inc()
                     continue
             if dist[i] > threshold:
+                hist_count += 1
                 s["anomaly"] = 1
                 logging.warning("Anomaly found (score: %f): %s" % (dist[i], s["message"]))
-
             else:
                 s["anomaly"] = 0
+            # anomaly_status==1 means its an anomaly otherwise its not we may want to do some comparison.
+            ANOMALY_COUNT.labels(anomaly_status=s["anomaly"]).inc()
+
             if self.storage_adapter.FACT_STORE_URL is not "":
                 self.process_false_positives(data, dist, i, s)
+            ANOMALY_HIST.observe(hist_count)
             f.append(s)
         return f
 
@@ -131,6 +147,7 @@ class SomModelAdapter(BaseModelAdapter):
         stdd = meta_data[1]
         mean = meta_data[0]
         threshold = self.storage_adapter.INFER_ANOMALY_THRESHOLD * stdd + mean
+        THRESHOLD.set(threshold)
         logging.info("threshold for anomaly is of %f" % threshold)
         logging.info("Models loaded, running %d infer loops" % self.storage_adapter.INFER_LOOPS)
         return mean, threshold
