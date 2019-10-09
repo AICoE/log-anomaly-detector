b801bd555bbb25e83260a1cf6f4bb8ee3465a317
Zak Hassan <zak.hassan1010@gmail.com>
2019-10-09 16:55:45 -0400

    Added release version to quay image

--- scripts/build-s2i-push-quay.sh
+++ scripts/build-s2i-push-quay.sh
@@ -1,4 +1,6 @@
+VERSION="0.0.1b5"
+
 s2i build https://github.com/AICoE/log-anomaly-detector.git  docker.io/centos/python-36-centos7  log-anomaly-detector-image
-docker tag log-anomaly-detector-image quay.io/aiops/log-anomaly-detector
-docker push quay.io/aiops/log-anomaly-detector
+docker tag log-anomaly-detector-image quay.io/aiops/log-anomaly-detector:$VERSION
+docker push quay.io/aiops/log-anomaly-detector:$VERSION
 
