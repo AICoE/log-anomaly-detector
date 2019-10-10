7aca6c2463c61e0601185a82558df7e185fa504b
Zak Hassan <zak.hassan1010@gmail.com>
2019-10-10 15:02:20 -0400

    Adding updates to build script

--- scripts/build-s2i-push-quay.sh
+++ scripts/build-s2i-push-quay.sh
@@ -1,6 +1,6 @@
-VERSION="0.0.1b5"
+VERSION="v0.1.0-beta.5"
 
-s2i build https://github.com/AICoE/log-anomaly-detector.git  docker.io/centos/python-36-centos7  log-anomaly-detector-image
+s2i build https://github.com/AICoE/log-anomaly-detector.git  registry.access.redhat.com/ubi8/python-36  log-anomaly-detector-image
 docker tag log-anomaly-detector-image quay.io/aiops/log-anomaly-detector:$VERSION
 docker push quay.io/aiops/log-anomaly-detector:$VERSION
 
