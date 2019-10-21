90ea9bce6a7972fc23678ce334957d16ba562880
Humair Khan <humair88@hotmail.com>
2019-10-21 17:07:34 -0400

    added es version config and separation logic to es_storage

--- anomaly_detector/storage/es_storage.py
+++ anomaly_detector/storage/es_storage.py
@@ -121,8 +121,10 @@ class ElasticSearchDataSource(StorageSource, DataCleaner, ESStorage):
         query["query"]["bool"]["must"][0]["query_string"]["query"] = self.config.ES_QUERY
 
         es_data = self.es.search(index_in, body=json.dumps(query))
-        if es_data["hits"]["total"] == 0 or es_data["hits"]["total"]["value"] == 0:
+        if (self.config.ES_VERSION < 7 and es_data["hits"]["total"] == 0) or \
+                (self.config.ES_VERSION >= 7 and es_data["hits"]["total"]["value"] == 0):
             return pandas.DataFrame(), es_data
+
         # only use _source sub-dict
         es_data = [x["_source"] for x in es_data["hits"]["hits"]]
         es_data_normalized = pandas.DataFrame(json_normalize(es_data)["message"])
